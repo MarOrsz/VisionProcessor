a5a10d6cd180c581e8318c83e9d7012b114d8a61
Marcin Orszulak <marcinorszulak8@gmail.com>
2025-04-29 16:02:44 +0200

    OpenCV working

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
-#include <fmt/core.h>
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
-int main()
-{
-    fmt::print("Hello World!\n");
+int main() {
+    // Wczytanie obrazu z pliku
+    cv::Mat image = cv::imread("obraz.jpg");
+
+    // Sprawdzenie, czy obraz został poprawnie wczytany
+    if (image.empty()) {
+        std::cout << "Nie udało się wczytać obrazu!" << std::endl;
+        return -1;
+    }
+
+    // Wyświetlenie obrazu w oknie
+    cv::imshow("Obraz", image);
+
+    // Oczekiwanie na naciśnięcie klawisza
+    cv::waitKey(0);
+
+    // Zamknięcie wszystkich okien
+    cv::destroyAllWindows();
+
     return 0;
 }
